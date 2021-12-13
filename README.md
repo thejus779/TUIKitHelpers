29986eb7095e1e1f4b2e893c4d0f3479fbfc7e4c
thejus thejus <t.thejus@useradgents.com>
2021-12-13 15:34:56 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TUIKitHelpers
+
+A description of this package.
---
+++ Tests/TUIKitHelpersTests/TUIKitHelpersTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TUIKitHelpers
+
+final class TUIKitHelpersTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TUIKitHelpers().text, "Hello, World!")
+    }
+}
